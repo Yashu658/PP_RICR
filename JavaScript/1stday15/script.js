412ca6f1b985976e838bd9a655ecdd76f18e14a9
Yashu Sharma <yashus658@gmail.com>
2025-02-16 16:47:30 +0530

    regular expression

--- JavaScript/1stday15/script.js
+++
@@ -1,9 +0,0 @@
-function changecolo1("con1"){
-    document.getElementById(con1).style.backgroundColor =color;
-}
-function changecolo2("h1"){
-    document.getElementById(con1).style.backgroundColor =color;
-}
-function changecolo3("p"){
-    document.getElementById(con1).style.backgroundColor = color;
-}
